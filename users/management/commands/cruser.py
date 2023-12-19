c935080e6dc6934eb868037d396a93a91de59355
taraassov <taraassov03@gmail.com>
2023-12-19 19:05:48 +0500

    commands

---
+++ users/management/commands/cruser.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='your_email',
+            first_name='name',
+            is_superuser=False,
+            is_staff=False,
+            is_active=True,
+        )
+
+        user.set_password('qazwsx2020g3')
+        user.save()
